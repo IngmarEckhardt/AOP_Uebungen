7fb35bc30d5afaa2005ff70fb3d69c151c456061
IngmarEckhardt <Eckhardt.Ingmar@gmx.de>
2021-10-28 18:02:11 +0200

    added problem 7 and cleaned code

--- eulerproblems.cpp
+++ eulerproblems.cpp
@@ -8,26 +8,33 @@ using std::cin;
 using std::endl;
 using std::setw;
 
-void eulerOne() {
+long int eulerOne()
+{
     int product;
     long int sum;
 
-    for (int i = 1; i < 200; i++) {
+    for (int i = 1; i < 200; i++)
+    {
         product = 5 * i;
         if (product % 3 != 0)
             sum += product;       //addiert die Vielfachen von 5, solange sie nicht durch 3 teilbar sind
     }
-    for (int i = 1; i < 334; i++) {
+    for (int i = 1; i < 334; i++)
+    {
         product = 3 * i;
         sum += product;                             //addiert alle vielfachen von drei auf die Summe drauf
     }
     cout << "Die Summe aller Zahlen bis 1000 die durch 3 oder 5 teilbar sind beträgt: " << sum << endl;
+
+    return sum;
 }
 
-void eulerTwo() {
-    int fib1 = 1, fib2 = 1, fibResult;
-    long int sum = 0;
-    do {
+long int eulerTwo()
+{
+    int fib1{1}, fib2{1}, fibResult;
+    long int sum{0};
+    do
+    {
         fibResult = fib1 + fib2;
         fib1 = fib2;
         fib2 = fibResult;
@@ -35,28 +42,37 @@ void eulerTwo() {
             sum += fibResult;   //addiert die aktuelle Fibonacci-Zahl zur Summe, falls sie gerade ist
     } while (fibResult <= 4'000'000);
     cout << "Die Summe der geraden Fibonacci-Zahlen kleiner 4.000.000 beträgt: " << sum << endl;
+
+    return sum;
 }
 
-void eulerThree() {
+long int eulerThree()
+{
     cout << "Prime-Factorizing of n = 600851475143" << endl;
     long long int product{
             600851475143};        //ausgangswert der Zerlegung, später als Zwischenergebnis zum Merken der Quotienten
     long int factor;                            //Merkzettel für den größten gefundenen Faktor
     for (int i = 2; product >
-                    1; i++) {         //erhöht i in jedem Durchlauf um die Primefaktoren zu finden, solange der Quotient nicht eins ist
+                    1; i++)
+    {         //erhöht i in jedem Durchlauf um die Primefaktoren zu finden, solange der Quotient nicht eins ist
         while (product % i ==
-               0) {              //Schleife sorgt dafür dass die For-Schleife dass der aktuelle Primefaktor i mehrfach genutzt wird
+               0)
+        {              //Schleife sorgt dafür dass die For-Schleife dass der aktuelle Primefaktor i mehrfach genutzt wird
             product /= i;                       //dividiert die Ausgangswert bzw die sich dann ergebenden Quotienten durch den Primefaktor, speichert den Quotienten als Zwischenschritt in Product
             factor = i;                         //merkt sich den letzten verwendeten Primefactor
         }
     }
     cout << "Der größte Primfaktor ist: " << factor << endl;
+
+    return factor;
 }
 
-int reverse_number(int num) {
+int reverse_number(int num)
+{
     int base = 10, reversed = 0;
 
-    while (num != 0) {
+    while (num != 0)
+    {
         int digit = num % base;                 //teilt die Ausgangszahl durch 10 und merkt sich den Rest
         reversed = reversed * base +
                    digit;     //multipliziert die Zwischenwerte der umgedrehten Zahl mit 10 und hängt den Rest an
@@ -65,24 +81,32 @@ int reverse_number(int num) {
     return reversed;
 }
 
-void eulerFour() {
+long int eulerFour()
+{
     long int product, intermediateResult;
-    for (int firstNumber = 999; firstNumber > 900; firstNumber--) {
-        for (int secondNumber = 999; secondNumber > 900; secondNumber--) {
+    for (int firstNumber = 999; firstNumber > 900; firstNumber--)
+    {
+        for (int secondNumber = 999; secondNumber > 900; secondNumber--)
+        {
             intermediateResult = firstNumber * secondNumber;
-            if ((intermediateResult > product) && (intermediateResult == reverse_number(intermediateResult))) {
+            if ((intermediateResult > product) && (intermediateResult == reverse_number(intermediateResult)))
+            {
                 product = intermediateResult;
             }
         }
     }
     cout << "Das größte Palindrom zweier dreistelliger Faktoren ist: " << product << endl;
+
+    return product;
 }
 
-void eulerFive() {
+long int eulerFive()
+{
     long int product;
     int i = 0;
     bool flag = false;
-    do {
+    do
+    {
         i++;
         product = i * 20;
         if (product % 19 != 0 || product % 18 != 0 || product % 17 != 0 || product % 16 != 0 || product % 15 != 0 ||
@@ -92,21 +116,53 @@ void eulerFive() {
         else flag = true;
         cout << "Die kleinste Zahl die ohne Rest durch alle Zahlen von 1 bis 20 teilbar ist, ist: " << product << endl;
     } while (!flag);
+    return product;
 }
 
-void eulerSix() {
-    unsigned long int powOfSum, sumOfSquares;
+unsigned long int eulerSix()
+{
+    unsigned long int powOfSum, sumOfSquares, difference;
     unsigned short int digit{100};
-    powOfSum = pow(((pow(digit, 2) + digit)/ 2), 2);
-    sumOfSquares = (digit * (digit+1)*(2*digit+2))/6;
-
+    powOfSum = pow(((pow(digit, 2) + digit) / 2), 2);
+    sumOfSquares = (digit * (digit + 1) * (2 * digit + 2)) / 6;
+    difference = (powOfSum - sumOfSquares);
     cout
             << "Die Differenz zwischen der quadratieren Summe aller Zahlen bis 100 und der Summe der Quadrate dieser Zahlen ist:\n"
             << (powOfSum - sumOfSquares) << endl;
+    return difference;
 }
 
-namespace euler {
-    void menu() {
+bool isPrime(long int numberToCheck)
+{
+    for (long int i = 2; i*i <= numberToCheck; i++)
+    {
+        if ((numberToCheck % i) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+unsigned long int eulerSeven()
+{
+    unsigned short int countPrimes = 1;
+    unsigned long int index = 1;
+
+    while (countPrimes < 10001)
+    {
+        index +=2;
+        if (isPrime(index)) countPrimes++;
+    }
+    cout << "Die 10.001te Primzahl ist die: " << index << endl;
+    return index;
+}
+
+namespace euler
+{
+
+    void menu()
+    {
         int menu;
 
         cout << "\nWähle das Euler-Problem" << endl;
@@ -116,10 +172,12 @@ namespace euler {
         cout << setw(73) << "Problem Vier - das größte Palindrom (4)" << endl;
         cout << setw(72) << "Problem Fünf - die kleinste teilbare Zahl (5)" << endl;
         cout << setw(71) << "Problem Sechs - Quadratsumme minus summierte Quadrate (6)" << endl;
+        cout << setw(71) << "Problem Sieben- Die 10.001te Primzahl (7)" << endl;
         cout << setw(72) << "Zurück zum Hauptmenu (0)" << endl;
         cin >> menu;
 
-        switch (menu) {
+        switch (menu)
+        {
             case 1:
                 eulerOne();
                 euler::menu();
@@ -142,6 +200,10 @@ namespace euler {
                 break;
             case 6:
                 eulerSix();
+                euler::menu();
+                break;
+            case 7:
+                eulerSeven();
                 euler::menu();
                 break;
             case 0:
--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <math.h>
 #include "connector.h"
 
-#ifdef _WIN32 
+#ifdef _WIN32
+
 #include <Windows.h>
+
 #define mSetConsoleOutputCP(x) SetConsoleOutputCP(x)
 #else
 #define mSetConsoleOutputCP(x) 
@@ -16,27 +18,35 @@ using std::endl;
 using std::setw;
 int userInp;
 
-void fakultaet() {
+unsigned long long fakultaet()
+{
     unsigned long long fak = 1;
 
     cout << "Gib die Zahl n ein deren Fakultät berechnet werden soll: " << endl;
     cin >> userInp;
-    for (; userInp > 1; userInp--) fak *=userInp;
+    for (; userInp > 1; userInp--) fak *= userInp;
     cout << "Die Fakultät ist: " << fak << endl;
+
+    return fak;
 }
 
-void multiplicationTable() {
+int multiplicationTable()
+{
     cout << "Multiplikationstabelle: " << endl;
 
-    for (int i=1; i<11;i++) {
-        for (int j=1; j < 11; j++) {
-            cout << setw(5) << i*j;
+    for (int i = 1; i < 11; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            cout << setw(5) << i * j;
         }
         cout << endl;
     }
+    return 0;
 }
 
-void nullstellen() {
+double nullstellen()
+{
     double a, b, c, diskriminante, realNull1, realNull2, imagNull;
 
     cout << "Nullstellenberechnung der Funktion ax²+bx+c" << endl;
@@ -50,45 +60,51 @@ void nullstellen() {
 
     diskriminante = pow(b, 2) - 4 * a * c;
 
-    if (a == 0 && b == 0 && c == 0) {
+    if (a == 0 && b == 0 && c == 0)
+    {
         cout << "Alle Werte von x sind eine Nullstelle, es gibt unendlich viele Nullstellen" << endl;
-    }
-    else if (a==0 && b==0) {
+    } else if (a == 0 && b == 0)
+    {
         cout << "Die Funktion hat keine Nullstellen" << endl;
-    }
-    else if (a==0) {
-        realNull1 = -c/b;
+    } else if (a == 0)
+    {
+        realNull1 = -c / b;
         cout << "Die Nullstelle der Funktion liegt bei x = " << realNull1 << endl;
-    }
-    else if (diskriminante == 0) {
-        realNull1 = -b/(2*a);
+    } else if (diskriminante == 0)
+    {
+        realNull1 = -b / (2 * a);
         cout << "Die Nullstelle der Funktion liegt bei x = " << realNull1 << endl;
-    }
-    else if (diskriminante > 0) {
-        realNull1 = (-b+sqrt(diskriminante)) / (2 * a);
-        realNull2 = (-b-sqrt(diskriminante)) / (2 * a);
+    } else if (diskriminante > 0)
+    {
+        realNull1 = (-b + sqrt(diskriminante)) / (2 * a);
+        realNull2 = (-b - sqrt(diskriminante)) / (2 * a);
         cout << "Die Nullstellen der Funktion liegen bei x1 = " << realNull1 << "und x2 = " << realNull2 << endl;
-    }
-    else {
-        realNull1 = -b/(2*a);
+    } else
+    {
+        realNull1 = -b / (2 * a);
         imagNull = sqrt(abs(diskriminante)) / (2 * a);
         cout << "Die Nullstellen der Funktion liegen im Bereich der komplexen Zahlen." << endl;
         cout << "x1 = " << realNull1 << " + " << imagNull << "i" << endl;
         cout << "x2 = " << realNull1 << " - " << imagNull << "i" << endl;
     }
+    return realNull1;
 }
 
-void fibonacci() {
-    unsigned long long fib1=0, fib2=1, fibResult;
+unsigned long long fibonacci()
+{
+    unsigned long long fib1 = 0, fib2 = 1, fibResult;
 
     cout << "Es werden die Fibonacci-Zahlen ausgegeben, wähle wieviele davon ausgegeben werden sollen:" << endl;
     cin >> userInp;
-    for (int i =1; i <= userInp; i++) {
-        if (i==1){
+    for (int i = 1; i <= userInp; i++)
+    {
+        if (i == 1)
+        {
             cout << fib1 << ", ";
             continue;
         }
-        if (i==2) {
+        if (i == 2)
+        {
             cout << fib2 << ", ";
             continue;
         }
@@ -97,9 +113,11 @@ void fibonacci() {
         fib2 = fibResult;
         cout << fibResult << ", ";
     }
+    return fibResult;
 }
 
-void menue () {
+void menue()
+{
     int menu;
 
     cout << "Wähle das Program welches du ausführen möchtest" << endl;
@@ -111,7 +129,8 @@ void menue () {
     cout << setw(50) << "Programm beenden (0)" << endl;
     cin >> menu;
 
-    switch (menu) {
+    switch (menu)
+    {
         case 1:
             fakultaet();
             break;
@@ -136,7 +155,8 @@ void menue () {
     menue();
 }
 
-int main() {
+int main()
+{
     mSetConsoleOutputCP(CP_UTF8);
     menue();
 }
